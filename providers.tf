d528889e76b7fb5900e39f820f2fa2724ed5b830
jubran nassar <jubrann@spacelift.io>
2023-01-13 16:05:58 -0500

    removes pats

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["/mnt/workspace/profile"]
+  # shared_credentials_files = ["~/.aws/credentials"]
   # profile                  = "terraform"
 }
