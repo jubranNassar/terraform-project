dbd647b3a19b1d4ad806eb5216b41f93672a74da
jubran nassar <jubrann@spacelift.io>
2023-03-13 09:48:20 -0400

    adds spacelift provider

--- providers.tf
+++ providers.tf
@@ -3,6 +3,10 @@ terraform {
     aws = {
       source = "hashicorp/aws"
     }
+    spacelift = {
+      source = "spacelift-io/spacelift"
+      version = "1.1.3"
+    }
   }
 }
 
@@ -11,4 +15,8 @@ provider "aws" {
   region                   = "us-east-1"
   # shared_credentials_files = ["~/.aws/credentials"]
   # profile                  = "terraform"
+}
+
+provider "spacelift" {
+  
 }
