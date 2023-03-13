a779bc03f9f1c1a79c698403ca5abadeb4ff38f3
jubran nassar <jubrann@spacelift.io>
2023-03-13 10:07:36 -0400

    creates plan policy

---
+++ spacelift.tf
@@ -0,0 +1,5 @@
+resource "spacelift_policy" "check instance type" {
+  name = "Let's make sure the instance type is t2.micro"
+  body = file("./policies/plan/check-instance-type.rego")
+  type = "PLAN"
+}
