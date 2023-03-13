1f4be36fd76e77f3644e16a951546584a3697964
jubran nassar <jubrann@spacelift.io>
2023-03-13 10:10:10 -0400

    fixes type

--- spacelift.tf
+++ spacelift.tf
@@ -1,4 +1,4 @@
-resource "spacelift_policy" "check instance type" {
+resource "spacelift_policy" "check-instance-type" {
   name = "Let's make sure the instance type is t2.micro"
   body = file("./policies/plan/check-instance-type.rego")
   type = "PLAN"
