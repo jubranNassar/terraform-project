4f9783a2a151efabe17be9c35250a38ed1e01772
jubran nassar <jubrann@spacelift.io>
2023-03-29 10:00:42 -0400

    adds type to policy

--- spacelift.tf
+++ spacelift.tf
@@ -1,5 +1,16 @@
+
+### Plan policies ###
 resource "spacelift_policy" "check-instance-type" {
   name = "Let's make sure the instance type is t2.micro"
   body = file("./policies/plan/check-instance-type.rego")
   type = "PLAN"
+}
+
+
+### Push policies ###
+
+resource "spacelift_policy" "trigger-run-from-pr" {
+  name = "trigger run from pr"
+  body = file("./policies/push/trigger-run-from-pr.rego")
+  type = "PUSH"
 }
