695f2fb40ae8a21912b853247465b722ec800659
jubran nassar <jubrann@spacelift.io>
2023-04-10 15:43:09 -0400

    adds providers

---
+++ worker-pool/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    spacelift = {
+      source = "spacelift-io/spacelift"
+      version = "1.1.3"
+    }
+  }
+}
+
+
+provider "aws" {
+  region                   = "us-east-1"
+  # shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "terraform"
+}
+
+provider "spacelift" {
+}
