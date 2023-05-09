9fdf6c02a2de91d00de88c8d5aacce02ab3249d0
jubran nassar <jubrann@spacelift.io>
2023-05-08 20:29:18 -0400

    fixes github block

--- administrative/stacks.tf
+++ administrative/stacks.tf
@@ -2,4 +2,7 @@ resource "spacelift_stack" "terraform-project" {
     branch = "main"
     name = "terraform-project"
     repository = "terraform-project"
+    github_enterprise {
+        namespace = "jubranNassar"
+    }
 }
