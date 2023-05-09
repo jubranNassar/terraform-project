db27613bf493f4caf274e3687f8c622197ce425a
jubran nassar <jubrann@spacelift.io>
2023-05-08 20:25:03 -0400

    removes github block

--- administrative/stacks.tf
+++ administrative/stacks.tf
@@ -2,7 +2,4 @@ resource "spacelift_stack" "terraform-project" {
     branch = "main"
     name = "terraform-project"
     repository = "terraform-project"
-    github_enterprise = {
-        namespace = jubranNassar
-    }
 }
