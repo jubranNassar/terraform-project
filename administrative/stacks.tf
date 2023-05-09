89448550831b7033f12df59f246abbd41cff2971
jubran nassar <jubrann@spacelift.io>
2023-05-08 20:47:01 -0400

    adds project root

--- administrative/stacks.tf
+++ administrative/stacks.tf
@@ -2,6 +2,7 @@ resource "spacelift_stack" "terraform-project" {
     branch = "main"
     name = "terraform-project"
     repository = "terraform-project"
+    project_root = "first-project"
     github_enterprise {
         namespace = "jubranNassar"
     }
