43955e69511b0bb9a7519adca4bcac7973993e3f
jubran nassar <jubrann@spacelift.io>
2023-05-08 21:19:21 -0400

    fixes path to key

--- administrative/contexts.tf
+++ administrative/contexts.tf
@@ -1,5 +1,5 @@
 resource "spacelift_mounted_file" "public-key" {
   stack_id      = "01GZYZVECGN71MSNY9HJ8YPGR2"
   relative_path = "public-key"
-  content       = filebase64("${path.module}/public-key.json")
+  content       = filebase64("${path.module}/jubran-dev-key.pub")
 }
