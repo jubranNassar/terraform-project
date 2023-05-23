8f14cfc64e91a03dd1672a4581cef612ff428d4c
jubran nassar <jubrann@spacelift.io>
2023-05-22 21:35:08 -0400

    fixes timezone and enables smart sanitization

--- administrative/stacks.tf
+++ administrative/stacks.tf
@@ -3,6 +3,7 @@ resource "spacelift_stack" "terraform-project" {
     name = "terraform-project"
     repository = "terraform-project"
     project_root = "first-project"
+    terraform_smart_sanitization = true
     worker_pool_id = "01H123RBHVK3GAX2KSTFC5R5WP"
     github_enterprise {
         namespace = "jubranNassar"
@@ -13,7 +14,7 @@ resource "spacelift_drift_detection" "terraform-project-drift" {
   reconcile = true
   stack_id  = spacelift_stack.terraform-project.id
   schedule  = ["*/15 * * * *"] # Every 15 minutes
-  timezone = "New_York EDT"
+  timezone = "America/New_York EDT"
 }
 
 
@@ -22,6 +23,7 @@ resource "spacelift_stack" "worker-pool" {
     name = "worker-pool"
     repository = "terraform-project"
     project_root = "worker-pool"
+    terraform_smart_sanitization = true
     worker_pool_id = "01H123RBHVK3GAX2KSTFC5R5WP"
     github_enterprise {
         namespace = "jubranNassar"
