97ead41051101abc21c5e9ca4d97a0b794a5aeb2
jubran nassar <jubrann@spacelift.io>
2023-12-21 12:32:40 -0500

    changes subnet

--- first-project/main.tf
+++ first-project/main.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "dev_public_subnet" {
 
 resource "aws_subnet" "dev_public_subnet-2" {
   vpc_id                  = aws_vpc.dev_vpc.id
-  cidr_block              = "10.0.128.0/17"
+  cidr_block              = "10.2.0.0/16"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
 
